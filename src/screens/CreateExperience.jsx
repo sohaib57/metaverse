c7ba4eb8fe4f2a52e96552a992669e7b63574da2
Sohaib Ashraf Butt <sohaibbutt57@gmail.com>
2022-05-13 13:01:04 +0500

    routes done

---
+++ src/screens/CreateExperience.jsx
@@ -0,0 +1,14 @@
+import { React, ReactFragment } from 'react'
+import { Stack } from '@chakra-ui/react'
+import Header from '../components/createexperience/Header'
+
+const CreateExperience = () => {
+    return (
+        <ReactFragment>
+            <Header />
+        </ReactFragment>
+
+    )
+}
+
+export default CreateExperience
