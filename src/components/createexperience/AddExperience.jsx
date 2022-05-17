9f8d09678d1997f54be9cb68e4b0418588b969bc
Sohaib Ashraf Butt <sohaibbutt57@gmail.com>
2022-05-17 18:06:35 +0500

    responsive done

--- src/components/createexperience/AddExperience.jsx
+++ src/components/createexperience/AddExperience.jsx
@@ -6,12 +6,12 @@ const AddExperience = () => {
 
 
     return (
-        <Stack flexDirection={'row'} h={'100vh'} margin={'0 !important'} width={'full'}  >
+        <Stack flexDirection={{ base: 'column', lg: 'row' }} h={'100vh'} margin={'0 !important'} width={'full'}  >
             <Stack margin={'0px !important'} width={'80%'} height={'full'} overflow={'auto'}   >
                 <CanvaComponent />
             </Stack>
 
-            <Stack margin={'0px !important'} height={'100%'} width={'20%'} bg='rgba(0, 0, 0, 0.03)'   >
+            <Stack margin={'0px !important'} pt={{ base: '20', lg: '0' }} height={'100%'} width={{ base: '100%', lg: '20%' }} bg='rgba(0, 0, 0, 0.03)'   >
                 <SidebarSetting />
             </Stack>
 
